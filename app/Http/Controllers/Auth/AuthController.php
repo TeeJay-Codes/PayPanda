c7300e8c71908ae4795994389c9df76c09081e9e
ADETUNJI OPAYELE <lordwhizy@gmail.com>
2015-11-08 08:50:25 +0100

    create auth interface

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,6 +23,10 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+    protected $redirectTo   = '/dashboard';
+    protected $loginPath    = '/login';
+    protected $username     = 'username';
+
     /**
      * Create a new authentication controller instance.
      *
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.dashboard.main')
+
+@section('title')DashBoard @stop
+
+@section('content')
+
+        <!-- Page Content -->
+<div class="content">
+    <!-- My Block -->
+    <div class="block">
+        <div class="block-header">
+            <ul class="block-options">
+                <li>
+                    <button type="button"><i class="si si-settings"></i></button>
+                </li>
+                <li>
+                    <button type="button" data-toggle="block-option" data-action="fullscreen_toggle"></button>
+                </li>
+                <li>
+                    <button type="button" data-toggle="block-option" data-action="refresh_toggle" data-action-mode="demo"><i class="si si-refresh"></i></button>
+                </li>
+                <li>
+                    <button type="button" data-toggle="block-option" data-action="content_toggle"></button>
+                </li>
+                <li>
+                    <button type="button" data-toggle="block-option" data-action="close"><i class="si si-close"></i></button>
+                </li>
+            </ul>
+            <h3 class="block-title">My Block</h3>
+        </div>
+        <div class="block-content">
+            <p>...</p>
+        </div>
+    </div>
+    <!-- END My Block -->
+</div>
+<!-- END Page Content -->
+</main>
+<!-- END Main Container -->
+
+@stop
